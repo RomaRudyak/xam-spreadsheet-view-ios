0688de55c511f3fd72815010177fdb25f05f7d95
Roma Rudyak <roma.rudyak@gmail.com>
2019-03-15 13:39:17 +0200

    Add ZMJClassData Sample
    Load data from file and sort it

---
+++ samples/ZMJClassData/Cells.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using CoreGraphics;
+using Spreadsheet;
+using UIKit;
+
+namespace ZMJClassData
+{
+    public abstract class CellBase : ZMJCell
+    {
+        private Boolean _disposed;
+        private UILabel _label;
+
+        public UILabel Label => _label;
+
+        protected CellBase(
+            IntPtr handle,
+            UIColor bgColor,
+            UIColor textColor,
+            nfloat fontSize)
+            : base(handle)
+        {
+            _label = new UILabel
+            {
+                Frame = Bounds,
+                AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight,
+                BackgroundColor = bgColor,
+                Font = UIFont.SystemFontOfSize(fontSize),
+                TextColor = textColor,
+                TextAlignment = UITextAlignment.Left,
+                Lines = 2
+            };
+
+            AddSubview(_label);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && !_disposed)
+            {
+                _disposed = true;
+                _label.RemoveFromSuperview();
+                _label.Dispose();
+                _label = null;
+            }
+            base.Dispose(disposing);
+        }
+    }
+
+    class HeaderCell : CellBase
+    {
+        private bool _disposed = false;
+
+        public UILabel SortArrow { get; private set; }
+
+        public HeaderCell(IntPtr handle)
+            : base(
+                handle,
+                UIColor.Clear,
+                UIColor.Black,
+                14)
+        {
+            SortArrow = new UILabel();
+            SortArrow.Font = UIFont.BoldSystemFontOfSize(14);
+            SortArrow.TextAlignment = UITextAlignment.Center;
+
+            AddSubview(SortArrow);
+        }
+
+        public override CGRect Frame
+        {
+            set
+            {
+                base.Frame = value;
+                Label.Frame = new CGRect(Bounds.Location, Bounds.Size).Inset(4, 2);
+            }
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+
+            SortArrow.SizeToFit();
+            SortArrow.Frame = new CGRect(
+                new CGPoint(
+                    Frame.Size.Width - SortArrow.Frame.Size.Width - 8,
+                    (Frame.Size.Height / 2) - SortArrow.Frame.Size.Height
+                ),
+                SortArrow.Frame.Size
+            );
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && !_disposed)
+            {
+                _disposed = true;
+                SortArrow.RemoveFromSuperview();
+                SortArrow.Dispose();
+                SortArrow = null;
+            }
+            base.Dispose(disposing);
+        }
+    }
+
+    class TextCell : CellBase
+    {
+        public UILabel SortArrow { get; private set; }
+
+        public TextCell(IntPtr handle)
+            : base(
+                handle,
+                new UIColor(0.0f, 0.0f, 1.0f, 0.2f),
+                UIColor.White,
+                14)
+        {
+            SelectedBackgroundView = ContentView;
+        }
+
+        public override CGRect Frame
+        {
+            set
+            {
+                base.Frame = value;
+                Label.Frame = new CGRect(Bounds.Location, Bounds.Size).Inset(4, 2);
+            }
+        }
+    }
+    }
---
+++ samples/ZMJClassData/ViewController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.IO;
+using CoreGraphics;
+using Foundation;
+using Spreadsheet;
+using UIKit;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ZMJClassData
+{
+    public enum ZMJSorting
+    {
+        ZMJAscending = 0,
+        ZMJDsescending
+    }
+
+    public struct SortedColumn
+    {
+        public SortedColumn(int column, ZMJSorting sorting) : this()
+        {
+            Column = column;
+            Sorting = sorting;
+        }
+
+        public Int32 Column { get; set; }
+        public ZMJSorting Sorting { get; set; }
+    }
+
+    public partial class ViewController : UIViewController
+        , ISpreadsheetViewDataSource
+        , ISpreadsheetViewDelegate
+    {
+        private SpreadsheetView _spreadsheetView;
+        private SortedColumn _sortedColumn;
+        private List<string[]> _data;
+        private string[] _header;
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            SetupViews();
+
+            _sortedColumn = new SortedColumn(0, ZMJSorting.ZMJAscending);
+
+            _data = File.ReadAllLines("data.tsv", Encoding.UTF8)
+                .Select(r => r.Split('\t'))
+                .ToList();
+
+            _header = _data.First();
+            _data.RemoveAt(0);
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+            _spreadsheetView.FlashScrollIndicators();
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+            _spreadsheetView.Frame = View.SafeAreaLayoutGuide.LayoutFrame;
+        }
+
+        static string GetSymbol(ZMJSorting sorting)
+        {
+            switch (sorting)
+            {
+                case ZMJSorting.ZMJAscending:
+                    return "\u25B2";
+                case ZMJSorting.ZMJDsescending:
+                    return "\u25BC";
+                default:
+                    return null;
+            }
+        }
+
+
+        private void SetupViews()
+        {
+            _spreadsheetView = new SpreadsheetView
+            {
+                AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+            };
+
+            _spreadsheetView.DataSource = this;
+            _spreadsheetView.Delegate = this;
+
+
+            _spreadsheetView.RegisterClassForCellReuse(typeof(HeaderCell), new NSString(nameof(HeaderCell)));
+            _spreadsheetView.RegisterClassForCellReuse(typeof(TextCell), new NSString(nameof(TextCell)));
+
+
+            Add(_spreadsheetView);
+        }
+
+        nint ISpreadsheetViewDataSource.NumberOfColumns(SpreadsheetView spreadsheetView) => _header.Length;
+
+        nint ISpreadsheetViewDataSource.NumberOfRows(SpreadsheetView spreadsheetView) => _data.Count + 1;
+
+        nfloat ISpreadsheetViewDataSource.WidthForColumn(SpreadsheetView spreadsheetView, nint column) => 140f;
+
+        nfloat ISpreadsheetViewDataSource.HeightForRow(SpreadsheetView spreadsheetView, nint row) => row == 0 ? 60f : 40f;
+
+        ZMJCell ISpreadsheetViewDataSource.CellForItemAt(SpreadsheetView spreadsheetView, NSIndexPath indexPath)
+        {
+            if (indexPath.Row == 0)
+            {
+                HeaderCell headeCell = (HeaderCell)spreadsheetView.DequeueReusableCellWithReuseIdentifier(nameof(HeaderCell), indexPath);
+                headeCell.Label.Text = _header[indexPath.GetColumn()];
+
+                if (indexPath.GetColumn() == _sortedColumn.Column)
+                {
+                    headeCell.SortArrow.Text = GetSymbol(_sortedColumn.Sorting);
+                    return headeCell;
+                }
+
+                headeCell.SortArrow.Text = "";
+                return headeCell;
+            }
+
+
+            TextCell cell = (TextCell)spreadsheetView.DequeueReusableCellWithReuseIdentifier(nameof(TextCell), indexPath);
+            cell.Label.Text = _data[indexPath.Row - 1][indexPath.GetColumn()];
+            return cell;
+        }
+
+        ZMJCellRange[] ISpreadsheetViewDataSource.MergedCells(SpreadsheetView spreadsheetView) => ZMJCellRange.None;
+
+        nint ISpreadsheetViewDataSource.FrozenColumns(SpreadsheetView spreadsheetView) => 0;
+
+        nint ISpreadsheetViewDataSource.FrozenRows(SpreadsheetView spreadsheetView) => 1;
+
+        [Export("spreadsheetView:didSelectItemAt:")]
+        public void DidSelectItemAt(SpreadsheetView spreadsheetView, NSIndexPath indexPath)
+        {
+            if (indexPath.Row != 0)
+            {
+                return;
+            }
+
+            if (_sortedColumn.Column == indexPath.GetColumn())
+            {
+                SortedColumn sc = _sortedColumn;
+                sc.Sorting = _sortedColumn.Sorting == ZMJSorting.ZMJAscending
+                    ? ZMJSorting.ZMJDsescending
+                    : ZMJSorting.ZMJAscending;
+
+                _sortedColumn = sc;
+            }
+            else
+            {
+                _sortedColumn = new SortedColumn((int)indexPath.GetColumn(), ZMJSorting.ZMJAscending);
+            }
+
+            if (_sortedColumn.Sorting == ZMJSorting.ZMJAscending)
+            {
+                _data = _data
+                    .OrderBy(r => r[_sortedColumn.Column])
+                    .ToList();
+            }
+            else
+            {
+                _data = _data
+                    .OrderByDescending(r => r[_sortedColumn.Column])
+                .ToList();
+            }
+
+            spreadsheetView.ReloadData();
+
+        }
+    }
+}
